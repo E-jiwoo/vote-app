f35e15cd2a5ebc42767191915391271556357faa
E-jiwoo <livei07@naver.com>
2024-03-04 08:05:37 +0900

    init :: setting

---
+++ components/Title/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+
+const Index = () => {
+  return <Text style={styles.title}>아아아</Text>;
+};
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 40,
+    fontWeight: "600",
+    color: "black",
+    margin: 30,
+  },
+});
+
+export default Index;
